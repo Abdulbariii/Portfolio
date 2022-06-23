b318cb9840812d9c5ae4dba1afdf423baf529539
Abdulbari Qaisar <Abdulbari.112917@gmail.com>
2022-06-23 23:43:59 +0300

    Adding styles to queries in the skill section

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const sign = document.querySelector(".sign-1");
 const hrSkill = document.querySelector(".hr-skill");
 const sc2 = document.querySelector(".sc-2");
 const pyramid = document.querySelector(".multiple");
+
 img.addEventListener("mouseover", function () {
   hr1.style.transform = "rotate(0)";
   hr2.style.transform = "rotate(90deg)";
@@ -18,16 +19,20 @@ img.addEventListener("mouseleave", function () {
 
 sc.addEventListener("mouseover", function () {
   sign.style.transform = "rotate(45deg)";
+  hrSkill.style.width = "213px";
 });
 
 sc.addEventListener("mouseleave", function () {
   sign.style.transform = "rotate(0)";
+  hrSkill.style.width = "0";
 });
 
 sc2.addEventListener("mouseover", function () {
   sign.style.transform = "rotate(45deg)";
+  hrSkill.style.width = "213px";
 });
 
 sc2.addEventListener("mouseleave", function () {
   sign.style.transform = "rotate(0)";
+  hrSkill.style.width = "0";
 });
