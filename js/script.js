bdd9ddfd3b64f36e8c8de91e75e36cde259b85b1
Abdulbari Qaisar <Abdulbari.112917@gmail.com>
2022-06-23 22:08:32 +0300

    Adding new card and with some animations with lines

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const img = document.querySelector(".home-container__img");
 const hr1 = document.querySelector(".hr");
 const hr2 = document.querySelector(".hr-0");
+const sc = document.querySelector(".skill-container");
+const sign = document.querySelector(".sign-1");
+const hrSkill = document.querySelector(".hr-skill");
+const sc2 = document.querySelector(".sc-2");
+const pyramid = document.querySelector(".multiple");
 img.addEventListener("mouseover", function () {
   hr1.style.transform = "rotate(0)";
   hr2.style.transform = "rotate(90deg)";
@@ -9,4 +14,20 @@ img.addEventListener("mouseover", function () {
 img.addEventListener("mouseleave", function () {
   hr1.style.transform = "rotate(5deg)";
   hr2.style.transform = "rotate(100deg)";
+});
+
+sc.addEventListener("mouseover", function () {
+  sign.style.transform = "rotate(45deg)";
+});
+
+sc.addEventListener("mouseleave", function () {
+  sign.style.transform = "rotate(0)";
+});
+
+sc2.addEventListener("mouseover", function () {
+  sign.style.transform = "rotate(45deg)";
+});
+
+sc2.addEventListener("mouseleave", function () {
+  sign.style.transform = "rotate(0)";
 });
